67c1f32ed7a6bedc6ae0f174b8cc9da6b61b8a43
Med Amine KHEBIB <133509651+BIBIBk4@users.noreply.github.com>
2025-01-17 15:10:14 +0100

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -2,13 +2,13 @@
 #define JEU_H
 
 #include <pthread.h>
+#include <ctype.h> 
 
-// Constantes
+#define TAILLE_MAX 100
 #define MAX_MOTS 100
-#define MAX_JOUEURS 10
-#define MAX_SYLLABES 500
+#define MAX_SYLLABES 100
+#define FICHIER_DICTIONNAIRE "dictionnaire.txt"
 
-// Variables globales
 extern pthread_mutex_t mutexMots;
 extern pthread_t threadBombe;
 extern int bombeTempsRestant;
@@ -19,10 +19,11 @@ extern char combinaisonActuelle[10];
 extern char motsUtilises[MAX_MOTS][50];
 extern int nbMotsUtilises;
 
-// Prototypes des fonctions
-void tourDeJeu();
 void genererCombinaison();
 int motEstValide(const char* mot);
 void ajouterMot(const char* mot);
+int checkMot(char* saisie);
+void convertir_en_majuscules(char *chaine);
+int mot_existe_dans_fichier(const char *mot, const char *nom_fichier);
 
 #endif
