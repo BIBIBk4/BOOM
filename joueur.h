0ca038d07e5d9d028acdb903e03a74695d9a54ee
hugo <hugo.bouquillon@gmail.com>
2025-03-17 10:40:08 +0100

    ajout types messages joueur

---
+++ joueur.h
@@ -0,0 +1,28 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <signal.h>
+
+// Prototypes des fonctions
+void demanderEtEnvoyerPseudo(int fileId);
+char *demanderReponse();
+void envoyerMessage(int fileId, char *message);
+void demanderPret();
+
+// Gestionnaires de signaux
+void pseudoInvalide(int sig);
+void motValide(int sig);
+void motInvalide(int sig);
+void aTonTour(int sig);
+void perteVie(int sig);
+void perdu(int sig);
+void gagne(int sig);
+
+#endif // JOUEUR_H
